c4a57ae0f7d0f6f1e9127fef98158c102c586653
Carlor Jr Diaz <cjunior90@gmail.com>
2024-03-01 21:24:35 -0500

    Ejercicio #8 resuelto

---
+++ Ejercicio8_NumeroParImpar/Program.cs
@@ -0,0 +1,12 @@
+﻿//Construir un programa que pida un número y luego diga si este número es par o no.
+//Declaramos las variables
+
+int num;
+string mensaje = "El número ingresado es ";
+
+//Realizamos la solicitud del número en la consola
+Console.Write("Por favor digite un número mayor a 0: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) Console.Write(mensaje + "par");
+else Console.Write(mensaje + "Impar");
