dac769206a6e9d2176ac83cc814832f00004ca2a
Carlor Jr Diaz <cjunior90@gmail.com>
2024-03-04 20:26:43 -0500

    Ejercicio 9 realizado

---
+++ Ejercicio9_NumMayor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Construir un programa que pida por pantalla 3 números y luego diga cuál es el mayor de los números ingresados.
+        // Solicitar numeros en pantalla
+
+        Console.Write("Ingrese primer numero: ");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese segundo numero: ");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese tercer numero: ");
+        double n3 = Convert.ToDouble(Console.ReadLine());
+
+        // Validar condiciones
+
+        if (((n1 > n2) && (n2 > n3))) Console.Write("El mayor es:" + n1);
+        else if (((n2 > n3) && (n3 > n1))) Console.Write("El mayor es: " + n2);
+        else Console.Write("El mayor es: " + n3);
+    }
+}
