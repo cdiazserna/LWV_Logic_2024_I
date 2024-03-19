dae5f7b49b0bd3c968c9d0971643ec1e3b11bf70
Carlor Jr Diaz <cjunior90@gmail.com>
2024-03-18 21:19:35 -0500

    Ejercicio 14 resuelo

---
+++ Ejercicio_14/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1, num2, numMenor, numMayor;
+        Console.Write("Digite el primer numero: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite el segundo numero: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+        numMenor = num2;
+        numMayor = num1;
+        }
+        else
+        {
+        numMenor = num1;
+        numMayor = num2;
+        }
+
+        Console.Write("\n");
+
+        for (int i = numMenor; i <= numMayor; i++)
+        Console.Write(i + " ");
+
+        Console.Write("\n");
+    }
+
+}
