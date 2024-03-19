0e2b13061835d2d322c0b99ba56d7cd5a3605cc0
Carlor Jr Diaz <cjunior90@gmail.com>
2024-03-18 20:49:31 -0500

    Ejercicio 16 resuelto con ciclo for

---
+++ Ejercicio16_CicloForNumeros/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Elabora un algoritmo que imprima los números del 1 al 100. Que calcule la suma de todos los números pares por un lado, y por otro, la de todos los impares.
+        //
+
+        //Declaración de variables
+        int acumuladorPar = 0, acumuladorImpar = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            //Mostramos los 100 primeros nums
+            Console.WriteLine(i);
+
+            //Identificar para acumular par o impar
+            if (i % 2 == 0) acumuladorPar += i;
+            else acumuladorImpar += i;
+
+            /*Forma inversa
+            if (i % 2 != 0) acumuladorImpar += i;
+            else acumuladorPar += i;*/
+        }
+
+        Console.WriteLine($"La suma de los pares es: {acumuladorPar}" );
+        Console.WriteLine($"La suma de los impares es: {acumuladorImpar}" );
+    }
+}
